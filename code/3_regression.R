b645f20aa37e4ac7cceb1ba16cdf3ad694525f1f
Lina Dimitrov <Lina.Dimitrov@emory.edu>
2026-04-23 19:26:56 -0400

    added a forest plot of model results

--- code/3_regression.R
+++ code/3_regression.R
@@ -21,5 +21,22 @@ model_table <- model %>%
   modify_caption("Linear regression predicting player all star status") %>%
   bold_labels()
 
+# -- Make model results into a forest plot
+
+tab <- broom::tidy(model, conf.int = TRUE, exponentiate = TRUE) %>%
+  filter(term != "(Intercept)") %>%   # remove intercept
+
+  ggplot(tab, aes(x = estimate, y = reorder(term, estimate))) +
+  geom_point(size = 3, color = "#1b4f72") +
+  geom_errorbarh(aes(xmin = conf.low, xmax = conf.high),
+                 height = 0.2, color = "#1b4f72") +
+  geom_vline(xintercept = 1, linetype = "dashed", color = "gray40") +
+  labs(
+    title = "Odds Ratios Predicting All-Star Status from Player Performance Stats",
+    x = "Odds Ratio (95% CI)",
+    y = ""
+  ) +
+  theme_minimal(base_size = 14)
+
 # -- Save table 
 saveRDS(model_table, "output/table3.rds")
